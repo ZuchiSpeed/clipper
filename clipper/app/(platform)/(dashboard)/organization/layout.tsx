526b58c97c71898c525bb4fba9c554a33d4c28a1
ZuchiSpeed <zuchi737@gmail.com>
2025-08-08 11:17:29 +0200

    completed settings page

--- clipper/app/(platform)/(dashboard)/organization/layout.tsx
+++ clipper/app/(platform)/(dashboard)/organization/layout.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "../_components/sidebar";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl max-auto">
+    <div className="pt-20 md:pt-1 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
       <div className="flex gap-x-7">
         <div className="w-64 shrink-0 hidden md:block">
           {/* Sidebar can be added here */}
